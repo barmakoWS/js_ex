c0668eb981453dd55f10c0c5af6fc9fa822f8a33
barmako <44745535+barmakoWS@users.noreply.github.com>
2022-02-15 09:49:43 +0200

    first commit

---
+++ ex2.js
@@ -0,0 +1,11 @@
+
+
+
+function f1(a, q){
+    return a(q) //f2()
+}
+
+function f2(t){
+    var db = pgp(“postgres://postgres:postgres@127.0.0.1” + “/” + “vulnerablenode”);
+    return db.one(t);
+}
